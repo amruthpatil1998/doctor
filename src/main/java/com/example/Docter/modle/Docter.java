8927755d2b6a3fc173184e57d1301de7ffbfe55c
amruthpatil1998 <126905878+amruthpatil1998@users.noreply.github.com>
2023-04-23 14:09:08 +0530

    Update Docter.java

--- src/main/java/com/example/Docter/modle/Docter.java
+++ src/main/java/com/example/Docter/modle/Docter.java
@@ -9,7 +9,7 @@ import javax.persistence.Table;
     @Entity
 	@Table(name = "docterApp")
 	public class Docter {
-
+//Abhishek
 		@Id
 		@GeneratedValue(strategy = GenerationType.AUTO)
 		private int eid;
